c3abf255199313545d63bb300d168f1928d1263a
ScaleShift <77553519+ScaleShiftX@users.noreply.github.com>
2025-08-31 08:14:47 -0700

    Add basic movement

--- godot/taxi/PlayerController.cs
+++ godot/taxi/PlayerController.cs
@@ -1,16 +1,14 @@
 using Godot;
 using System;
 
-public partial class PlayerController : RigidBody3D
+public partial class PlayerController : VehicleBody3D
 {
     [ExportCategory("Model")]
     [Export] private PackedScene _blend;
     private Node3D _blendInstance;
 
     [ExportCategory("Movement")]
-    [Export] public float AccelerationDPS2 = 3000f;
-    [Export] public float TurnRate = 6f;
-    [Export] public float AngularVelocityLinearInterpolationWeight = 0.2f;
+    [Export] private float _engineForce = 300f;
 
     public override void _Ready()
     {
@@ -21,5 +19,20 @@ public partial class PlayerController : RigidBody3D
         ////fix the .blend offsets
         //_blendInstance.Position = new Vector3(0f, 1.023f, 0f);
         //_blendInstance.RotationDegrees = new Vector3(0f, 90f, 0f);
+    }
+
+    public override void _PhysicsProcess(double deltaDouble)
+    {
+        float delta = (float)deltaDouble;
+
+        if (Input.IsActionPressed("thrust_dir_forward"))
+        {
+            GD.Print("Thurst forward");
+            EngineForce = _engineForce;
+        }
+        else
+        {
+            EngineForce = 0f;
+        }
     }
 }
--- godot/taxi/Wheel.cs
+++ godot/taxi/Wheel.cs
@@ -1,53 +1,53 @@
 using Godot;
 using System;
 
-public partial class Wheel : RigidBody3D
+public partial class Wheel : VehicleWheel3D
 {
-    [ExportCategory("External")]
-    [Export] private PlayerController _playerController;
-    [Export] private Camera3D _cameraPlayer;
-
-    [ExportCategory("Movement")]
-    [Export] private bool _isTurnable = false;
-    [Export] private bool _isAcceleratable = false;
-
-    public override void _IntegrateForces(PhysicsDirectBodyState3D state)
-    {
-        float delta = state.Step;
-
-        //Turn
-        if (_isTurnable)
-        {
-            Turn(state);
-        }
-    }
-
-    public override void _PhysicsProcess(double delta)
-    {
-        //Accelerate
-        if (_isAcceleratable && Input.IsActionPressed("thrust_dir_forward"))
-        {
-            ApplyTorque(GlobalBasis.X * _playerController.AccelerationDPS2);
-        }
-    }
-
-    private void Turn(PhysicsDirectBodyState3D state)
-    {
-        Vector3 targetPosition = new Vector3(_cameraPlayer.GlobalPosition.X, _playerController.GlobalPosition.Y, _cameraPlayer.GlobalPosition.Z);
-        
-        Vector3 direction = (targetPosition - _playerController.GlobalPosition).Normalized();
-        if (direction.LengthSquared() < 1e-6f) return; //distance too small to matter
-
-        Vector3 forward = -GlobalBasis.Z;
-
-        Vector3 axis = forward.Cross(direction);
-        float dot = forward.Dot(direction);
-        float angle = Mathf.Acos(dot);
-        if (angle < 1e-4f || axis.LengthSquared() < 1e-8f) return; //already aligned; also avoids NaN errors with axis
-        axis = axis.Normalized();
-
-        Vector3 desiredAV = axis * (angle * _playerController.TurnRate);
-
-        state.AngularVelocity = state.AngularVelocity.Lerp(desiredAV, _playerController.AngularVelocityLinearInterpolationWeight);
-    }
+    //[ExportCategory("External")]
+    //[Export] private PlayerController _playerController;
+    //[Export] private Camera3D _cameraPlayer;
+    //
+    //[ExportCategory("Movement")]
+    //[Export] private bool _isTurnable = false;
+    //[Export] private bool _isAcceleratable = false;
+    //
+    //public override void _IntegrateForces(PhysicsDirectBodyState3D state)
+    //{
+    //    float delta = state.Step;
+    //
+    //    //Turn
+    //    if (_isTurnable)
+    //    {
+    //        Turn(state);
+    //    }
+    //}
+    //
+    //public override void _PhysicsProcess(double delta)
+    //{
+    //    //Accelerate
+    //    if (_isAcceleratable && Input.IsActionPressed("thrust_dir_forward"))
+    //    {
+    //        ApplyTorque(GlobalBasis.X * _playerController.AccelerationDPS2);
+    //    }
+    //}
+    //
+    //private void Turn(PhysicsDirectBodyState3D state)
+    //{
+    //    Vector3 targetPosition = new Vector3(_cameraPlayer.GlobalPosition.X, _playerController.GlobalPosition.Y, _cameraPlayer.GlobalPosition.Z);
+    //    
+    //    Vector3 direction = (targetPosition - _playerController.GlobalPosition).Normalized();
+    //    if (direction.LengthSquared() < 1e-6f) return; //distance too small to matter
+    //
+    //    Vector3 forward = -GlobalBasis.Z;
+    //
+    //    Vector3 axis = forward.Cross(direction);
+    //    float dot = forward.Dot(direction);
+    //    float angle = Mathf.Acos(dot);
+    //    if (angle < 1e-4f || axis.LengthSquared() < 1e-8f) return; //already aligned; also avoids NaN errors with axis
+    //    axis = axis.Normalized();
+    //
+    //    Vector3 desiredAV = axis * (angle * _playerController.TurnRate);
+    //
+    //    state.AngularVelocity = state.AngularVelocity.Lerp(desiredAV, _playerController.AngularVelocityLinearInterpolationWeight);
+    //}
 }
