2ca71f435e3533e0a33579c75956de9397f042f0
ScaleShift <77553519+ScaleShiftX@users.noreply.github.com>
2025-09-05 10:36:58 -0700

    Add basic procedural road creation

--- godot/environment/road/Road.cs
+++ godot/environment/road/Road.cs
@@ -1,9 +1,34 @@
 using Godot;
 using System;
 
+[Tool]
 public partial class Road : Node
 {
-    public override void _Ready()
+    [ExportToolButton("Create Roads", Icon = "PackedScene")] public Callable ButtonCreate => Callable.From(CreateRoads);
+    [ExportToolButton("Clear Roads", Icon = "PackedScene")] public Callable ButtonClear => Callable.From(ClearRoads);
+
+    [Export] private Vector2I _dimensions = new Vector2I(2, 2);
+
+    [Export] private PackedScene _blendNorthSouth;
+    [Export] private PackedScene _blendEastWest;
+    [Export] private PackedScene _blendIntersection4Way;
+
+    private void CreateRoads()
+    {
+        var sceneOwner = GetTree().EditedSceneRoot;
+
+        for (int x = 0; x < _dimensions.X; x++)
+        {
+            for (int z = 0; z < _dimensions.Y; z++)
+            {
+                InstantiateFromToolAtPos(sceneOwner, _blendIntersection4Way, x, z);
+            }
+        }
+
+        CreateColliders();
+    }
+
+    private void CreateColliders()
     {
         foreach (Node blend in GetChildren()) //.blend inherited packed scenes
         {
@@ -19,5 +44,30 @@ public partial class Road : Node
                 meshInstance.CreateConvexCollision();
             }
         }
+    }
+
+    private void ClearRoads()
+    {
+        foreach (Node blend in GetChildren()) //.blend inherited packed scenes
+        {
+            blend.QueueFree();
+        }
+    }
+
+    private static void MakeOwnedRecursive(Node node, Node owner)
+    {
+        node.Owner = owner;
+        foreach (var child in node.GetChildren())
+        {
+            MakeOwnedRecursive(child, owner);
+        }
+    }
+
+    private void InstantiateFromToolAtPos(Node sceneOwner, PackedScene packedScene, float x, float z)
+    {
+        Node3D instance = (Node3D)packedScene.Instantiate();
+        AddChild(instance);
+        instance.GlobalPosition = new Vector3(x, 0f, z);
+        MakeOwnedRecursive(instance, sceneOwner);
     }
 }
