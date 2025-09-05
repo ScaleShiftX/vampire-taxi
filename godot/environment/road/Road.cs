57b2373142de6c0e3b83fcccdde63deaa34429e2
ScaleShift <77553519+ScaleShiftX@users.noreply.github.com>
2025-09-05 08:55:35 -0700

    Refactor Road.cs to be on parent node, fix east/west UVs
    - Setup Road.cs to be on parent node instead of every road (.blend inherited packed scene Node3D)
    - Fix UV scaling on east/west road

--- godot/environment/road/Road.cs
+++ godot/environment/road/Road.cs
@@ -1,16 +1,21 @@
 using Godot;
 using System;
 
-public partial class Road : Node3D
+public partial class Road : Node
 {
     public override void _Ready()
     {
-        //Create static colliders for all mesh children
-        foreach (Node child in GetChildren())
+        foreach (Node blend in GetChildren()) //.blend inherited packed scenes
         {
-            if (child is MeshInstance3D meshInstance)
+            //GD.Print($"blend: {blend.Name}");
+
+            //Create static colliders for all mesh children
+            foreach (Node mesh in blend.GetChildren()) //meshes
             {
+                //GD.Print($"mesh: {mesh.Name}");
+
                 //Create Static Body child and a Single Convex collider as a child of that
+                MeshInstance3D meshInstance = mesh as MeshInstance3D;
                 meshInstance.CreateConvexCollision();
             }
         }
